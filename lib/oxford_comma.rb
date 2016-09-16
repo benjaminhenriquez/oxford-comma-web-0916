d563c9e7d0f15d77709b0c9bcb6c70db4dd6a660
Benjamin Henriquez <henriquez.ben@gmail.com>
2016-09-16 23:50:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+  return array[0]
+elsif array.length == 2
+  return array.join(" and ")
+elsif array.length >= 3
+  string = array[0..(array.length - 2)].join(", ")
+  string = string + ", and #{array[-1]}"
+  return string
+end
 end
